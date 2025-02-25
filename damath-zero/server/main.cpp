c0de191ca470d484ead82cc60761027b537ebc51
Daniel David N. Bador <danieldavidbador@yahoo.com>
2025-02-25 20:52:56 +0800

    add server

--- damath-zero/server/main.cpp
+++ damath-zero/server/main.cpp
@@ -1,5 +1,51 @@
+#include <damath-zero.grpc.pb.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
 
-#include "damath-zero.pb.h"
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/strings/str_format.h"
 
-auto main() -> int { return 0; }
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using damath_zero::DamathZero;
+using damath_zero::GameAction;
+using damath_zero::GameState;
+
+ABSL_FLAG(uint16_t, port, 50051, "Server port for the service");
+
+class DamathZeroService final : public DamathZero::Service {
+  Status predict(ServerContext* context, const GameState* request,
+                 GameAction* reply) override {
+    std::string prefix("Hello ");
+    reply->set_message(prefix + request->name());
+    return Status::OK;
+  }
+};
+
+void RunServer(uint16_t port) {
+  std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
+  DamathZeroService service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  ServerBuilder builder;
+
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  absl::ParseCommandLine(argc, argv);
+  RunServer(absl::GetFlag(FLAGS_port));
+  return 0;
+}
