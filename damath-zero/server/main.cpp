d6ee3ab9ca0d5c9bf10d4116b8c724ef113df5b4
Daniel David N. Bador <danieldavidbador@yahoo.com>
2025-02-26 21:51:24 +0800

    add svelte

--- damath-zero/server/main.cpp
+++ damath-zero/server/main.cpp
@@ -1,14 +1,23 @@
 #include <damath-zero/core/mcts.h>
 #include <httplib.h>
 
+#include <filesystem>
+#include <print>
+
 auto main(int argc, char** argv) -> int {
   auto srv = httplib::Server{};
 
-  if (!srv.set_mount_point("/", "./"))
-    return 1;
+  auto path = std::string_view{argv[0]};
+  if (path.ends_with("server")) {
+    std::filesystem::current_path(path.substr(0, path.find_last_of("/\\")));
+  }
 
+  if (!srv.set_mount_point("/", std::filesystem::current_path() / "dist"))
+    std::print("Failed to set mount point...\n");
+
+  std::print("Listening on http://0.0.0.0:80\n");
   if (!srv.listen("0.0.0.0", 80))
-    return 1;
+    std::print("Failed to listen...\n");
 
   return 0;
 }
