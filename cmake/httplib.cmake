89545df50c963ef59790dc6aa89e2677b91a400e
Daniel David N. Bador <danieldavidbador@yahoo.com>
2025-02-26 16:12:54 +0800

    change server to cpp-httplib

---
+++ cmake/httplib.cmake
@@ -0,0 +1,4 @@
+FetchContent_Declare(httplib
+    GIT_REPOSITORY https://github.com/yhirose/cpp-httplib
+    GIT_TAG master)
+  FetchContent_MakeAvailable(httplib)
--- damath-zero/server/main.cpp
+++ damath-zero/server/main.cpp
@@ -1,51 +1,14 @@
-#include <damath-zero.grpc.pb.h>
-#include <grpcpp/ext/proto_server_reflection_plugin.h>
-#include <grpcpp/grpcpp.h>
-#include <grpcpp/health_check_service_interface.h>
+#include <damath-zero/core/mcts.h>
+#include <httplib.h>
 
-#include "absl/flags/flag.h"
-#include "absl/flags/parse.h"
-#include "absl/strings/str_format.h"
+auto main(int argc, char** argv) -> int {
+  auto srv = httplib::Server{};
 
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::Status;
+  if (!srv.set_mount_point("/", "./"))
+    return 1;
 
-using damath_zero::DamathZero;
-using damath_zero::GameAction;
-using damath_zero::GameState;
+  if (!srv.listen("0.0.0.0", 80))
+    return 1;
 
-ABSL_FLAG(uint16_t, port, 50051, "Server port for the service");
-
-class DamathZeroService final : public DamathZero::Service {
-  Status predict(ServerContext* context, const GameState* request,
-                 GameAction* reply) override {
-    std::string prefix("Hello ");
-    reply->set_message(prefix + request->name());
-    return Status::OK;
-  }
-};
-
-void RunServer(uint16_t port) {
-  std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
-  DamathZeroService service;
-
-  grpc::EnableDefaultHealthCheckService(true);
-  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
-  ServerBuilder builder;
-
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(&service);
-
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
-
-  server->Wait();
-}
-
-int main(int argc, char** argv) {
-  absl::ParseCommandLine(argc, argv);
-  RunServer(absl::GetFlag(FLAGS_port));
   return 0;
 }
