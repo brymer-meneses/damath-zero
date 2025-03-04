3859505de8470bf65a4d0a90709771e6bd227b11
Daniel David N. Bador <danieldavidbador@yahoo.com>
2025-03-04 18:35:32 +0800

    fix homepage

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,3 +1,8 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div> Hi there </div>;
+  return <div className="container mx-auto flex gap-4">
+    <Link href="/tictactoe">TicTacToe</Link>
+    <Link href="/damath">Damath</Link>
+  </div>
 }
